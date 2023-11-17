ae82e6937d488bcb12f72141eb6132c19d51a430
Ray Strode <rstrode@redhat.com>
2023-11-17 11:52:07 -0500

    add another script to watch for idle. this one session specific.

---
+++ watch-for-session-idle.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+coproc IDLE_MONITOR (busctl --user monitor org.gnome.SessionManager)
+
+# 3 means idle, it's normally 0 which means "active"
+
+until grep -q "3" <(busctl --user get-property org.gnome.SessionManager /org/gnome/SessionManager/Presence org.gnome.SessionManager.Presence status)
+do
+        read -u "${IDLE_MONITOR[0]}"
+done
+
+shift
+"$@"
+
