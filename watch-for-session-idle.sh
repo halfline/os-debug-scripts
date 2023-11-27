0fc2bda9040efcebc09acfbd0cceb1bbd69b8f6c
Ray Strode <rstrode@redhat.com>
2023-11-27 13:45:01 -0500

    watch-for-session-idle.sh: Run indefinitely
    At the moment watch-for-session-idle.sh exits after the first idle
    event. It's more useful if it runs continuously.
    
    This commit adds a `while true` loop around it.

--- watch-for-session-idle.sh
+++ watch-for-session-idle.sh
@@ -2,13 +2,15 @@
 
 coproc IDLE_MONITOR (busctl --user monitor org.gnome.SessionManager)
 
-# 3 means idle, it's normally 0 which means "active"
-
-until grep -q "3" <(busctl --user get-property org.gnome.SessionManager /org/gnome/SessionManager/Presence org.gnome.SessionManager.Presence status)
+while true
 do
-        read -u "${IDLE_MONITOR[0]}"
-done
+        # 3 means idle, it's normally 0 which means "active"
+        until grep -q "3" <(busctl --user get-property org.gnome.SessionManager /org/gnome/SessionManager/Presence org.gnome.SessionManager.Presence status)
+        do
+                read -u "${IDLE_MONITOR[0]}"
+        done
 
-shift
-"$@"
+        shift
+        "$@"
+done
 
