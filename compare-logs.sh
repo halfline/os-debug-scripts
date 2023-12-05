dcfb62ade468406432e575330bc3574d0d21fb19
Ray Strode <rstrode@redhat.com>
2023-12-05 15:26:07 -0500

    compare-logs.sh: Fix timestamp pattern

--- compare-logs.sh
+++ compare-logs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Compare $1 against $2
-readarray -t FIRST_ONLY < <(grep -v -F -f <(sed -e 's/^[0-9]*:[0-9]*:[0-9]* //g' -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $2) -- <(sed -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $1))
+readarray -t FIRST_ONLY < <(grep -v -F -f <(sed -e 's/^[0-9]*:[0-9]*:[0-9]*[.][0-9]* //g' -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $2) -- <(sed -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $1))
 
 # Compare $2 against $1
-readarray -t SECOND_ONLY < <(grep -v -F -f <(sed -e 's/^[0-9]*:[0-9]*:[0-9]* //g' -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $1) -- <(sed -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $2))
+readarray -t SECOND_ONLY < <(grep -v -F -f <(sed -e 's/^[0-9]*:[0-9]*:[0-9]*[.][0-9]* //g' -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $1) -- <(sed -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $2))
 
 # Print results if arrays are not empty
 if [ ${#FIRST_ONLY[@]} -ne 0 ]; then
