870b0fce13b6fa5ef489f73e492c86cd671b3885
Ray Strode <rstrode@redhat.com>
2023-12-05 16:09:35 -0500

    compare-logs: Do things manually
    I'm not trusting grep -v -f right now, just roll it myself.

--- compare-logs.sh
+++ compare-logs.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
+mask_descriptors() {
+        sed -e 's/0x[0-9a-fA-F]*\w/$pointer/g' \
+            -e 's/pid [0-9]*/pid $PID/g' \
+            -e 's@/proc/[0-9]*@/proc/$PID@g' \
+            -e 's/fd [0-9]*/fd $FD/g' \
+            $1
+}
+
+# Function to compare two files line by line
+compare_files() {
+        local -a result=()
+
+        while read -r line; do
+                needle=$(echo "$line" | sed -e 's/^[0-9]*:[0-9]*:[0-9]*[.][0-9]* //g')
+                if ! grep -q -F -- "$needle" <(mask_descriptors "$2"); then
+                        echo "${line}"
+                fi
+        done < <(mask_descriptors "$1")
+}
+
 # Compare $1 against $2
-readarray -t FIRST_ONLY < <(grep -v -F -f <(sed -e 's/^[0-9]*:[0-9]*:[0-9]*[.][0-9]* //g' -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $2) -- <(sed -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $1))
+readarray -t FIRST_ONLY < <(compare_files "$1" "$2")
 
-# Compare $2 against $1
-readarray -t SECOND_ONLY < <(grep -v -F -f <(sed -e 's/^[0-9]*:[0-9]*:[0-9]*[.][0-9]* //g' -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $1) -- <(sed -e 's/0x[0-9a-f]*\w/$pointer/g' -e 's/[0-9]/N/g' $2))
-
-# Print results if arrays are not empty
 if [ ${#FIRST_ONLY[@]} -ne 0 ]; then
-        echo "Lines in $1 but not in $2:"
+        echo -e "\nLines in $1 but not in $2:"
         printf "%s\n" "${FIRST_ONLY[@]}"
 fi
+
+# Compare $2 against $1
+readarray -t SECOND_ONLY < <(compare_files "$2" "$1")
 
 if [ ${#SECOND_ONLY[@]} -ne 0 ]; then
         echo -e "\nLines in $2 but not in $1:"
