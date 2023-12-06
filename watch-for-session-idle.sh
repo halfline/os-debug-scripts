e3ce281f7655fff577c9f61568e44cdb0778cac2
Ray Strode <rstrode@redhat.com>
2023-12-06 10:17:12 -0500

    wait-for-session-idle.sh: Be more robust against niche presence status
    I don't believe the "invisible" and "busy" statuses are really used
    anymore, nevertheless, this commit adds code to treat them as
    active statuses as well just for completeness sake.

--- watch-for-session-idle.sh
+++ watch-for-session-idle.sh
@@ -6,14 +6,21 @@ MATCH_FILTER="\
         path='/org/gnome/SessionManager/Presence'\
 "
 
+# Initialize the STATUS associative array
+declare -A STATUS=(
+        ['available']=0
+        ['invisible']=1
+        ['busy']=2
+        ['idle']=3
+)
+
 # Monitor session manager presence status changes announced over the bus in another process running alongside this one
 coproc IDLE_MONITOR (busctl --user monitor org.gnome.SessionManager --match "${MATCH_FILTER}" --json=short)
 
 while true
 do
         # Check if the user has gone idle
-        # 3 means idle, it's normally 0 which means "active"
-        until grep -q "3" <(busctl --user get-property org.gnome.SessionManager /org/gnome/SessionManager/Presence org.gnome.SessionManager.Presence status)
+        until grep -q "${STATUS['idle']}" <(busctl --user get-property org.gnome.SessionManager /org/gnome/SessionManager/Presence org.gnome.SessionManager.Presence status)
         do
                 # If not, wait idly until the other process reports more bus traffic from the session manager
                 read -u "${IDLE_MONITOR[0]}"
@@ -23,7 +30,7 @@ do
         "$@"
 
         # Command run, wait for the system to become active again
-        until grep -q "0" <(busctl --user get-property org.gnome.SessionManager /org/gnome/SessionManager/Presence org.gnome.SessionManager.Presence status)
+        while grep -q "${STATUS['idle']}" <(busctl --user get-property org.gnome.SessionManager /org/gnome/SessionManager/Presence org.gnome.SessionManager.Presence status)
         do
                 # If not active, wait idly until the other process reports more bus traffic from the session manager
                 read -u "${IDLE_MONITOR[0]}"
